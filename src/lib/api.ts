bb10b54818bf56c8ee455bbe637f18171cdafa5d
esraod <oktavesra@gmail.com>
2025-09-08 17:17:22 +0200

    API fetch for countres added

---
+++ src/lib/api.ts
@@ -0,0 +1,13 @@
+export type Country = {
+  cca3: string
+  name: { common: string }
+}
+
+export async function getAllCountries(): Promise<Country[]> {
+  const res = await fetch(
+    'https://restcountries.com/v3.1/all?fields=name,cca3',
+    { next: { revalidate: 86400 } } // cache i 1 dag
+  )
+  if (!res.ok) throw new Error('Failed to fetch countries')
+  return (await res.json()) as Country[]
+}
